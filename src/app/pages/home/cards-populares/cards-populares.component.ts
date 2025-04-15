34c1cde6bcfde28032ec8c02b48bd88a6de8c1dc
Jesus <alonstarkj@gmail.com>
2025-04-15 09:48:17 -0500

    Se agrego la sección de cursos populares con actualizaciones de estilo y contenido:
    Se implementó un nuevo componente para mostrar cursos populares.
    - Detalles actualizados del curso en la sección de cursos públicos, incluidos títulos, duraciones y descripciones.
    - Estilización mejorada para tarjetas de curso con efectos y animaciones de desplazamiento.
     - Se agregó Font Awesome para el uso de iconos en el botón flotante de WhatsApp.
    - Traducí algún texto al español para una mejor localización..

---
+++ src/app/pages/home/cards-populares/cards-populares.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-populares',
+  imports: [],
+  templateUrl: './cards-populares.component.html',
+  styleUrl: './cards-populares.component.scss'
+})
+export class CardsPopularesComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 // import LayoutLandingComponent from "../../layout/landing/layout-landing/layout-landing.component";
 import { CardsComponent } from "./cards/cards.component";
+import { CardsPopularesComponent } from "./cards-populares/cards-populares.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CardsComponent],
+  imports: [CardsComponent, CardsPopularesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
