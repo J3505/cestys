058de56b356c60a14c07c3f2eecf880e261ddd3a
Jesus <alonstarkj@gmail.com>
2025-04-21 17:48:57 -0500

    feat(home): Actualizar el texto de la sección principal y añadir un iframe de Google Maps para la ubicación. Refactorizar (cursos): Mejorar el diseño de las categorías de la barra lateral y las publicaciones recientes. Corregir (barra de navegación): Eliminar estilos innecesarios y mejorar la respuesta de la barra de navegación. Estilo (pie de página): Actualizar la fuente de la imagen del logotipo para un mejor rendimiento.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 // import LayoutLandingComponent from "../../layout/landing/layout-landing/layout-landing.component";
 import { CardsComponent } from "./cards/cards.component";
 import { CardsPopularesComponent } from "./cards-populares/cards-populares.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CardsComponent, CardsPopularesComponent],
+  imports: [CardsComponent, CardsPopularesComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
