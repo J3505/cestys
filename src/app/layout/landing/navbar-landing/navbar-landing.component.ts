a51a3dd20fc976bf5ecbd3685cc3c2cd2f726865
Jason <alonstarkj@gmail.com>
2025-04-30 17:40:01 -0500

    feat: actualizar logos en el pie de página y la barra de navegación, y corregir el título en el index.html

--- src/app/layout/landing/navbar-landing/navbar-landing.component.ts
+++ src/app/layout/landing/navbar-landing/navbar-landing.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-landing',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, RouterLinkActive],
   templateUrl: './navbar-landing.component.html',
   styleUrl: './navbar-landing.component.scss'
 })
 export class NavbarLandingComponent {
+
+  rutasHome = [
+    {label: 'Inicio', path: '/' },
+    {label: 'Cursos', path: '/cursos' },
+    {label: 'Nosotros', path: '/nosotros' },
+    {label: '+', path: '/' },
+  ]
+
+
   menuActivo = false;
 
   toggleMenu(): void {
