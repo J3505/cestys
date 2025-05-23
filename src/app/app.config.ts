d4e000c3758eab1194c3f93e1633d87a7ae6ccc0
Jason <alonstarkj@gmail.com>
2025-05-23 17:56:53 -0500

    feat: add custom theme preset and update app configuration; enhance paginator styling and structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import { MyPreset } from '../mypreset';
 import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,10 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
-          darkModeSelector: false || 'none',
-        },
+            darkModeSelector: false || 'none'
+        }
       },
     }),
   ],
---
+++ src/mypreset.ts
@@ -0,0 +1,56 @@
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+
+export const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50:  '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{orange.500}',
+          inverseColor: '#ffffff',
+          hoverColor: '{orange.600}',
+          activeColor: '{orange.800}',
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+        custom:{
+          background: '{orange.500}',
+          focusBackground: '{orange.600}',
+          color: '#ffffff',
+        }
+        
+      },
+      dark: {
+        primary: {
+          color: '{red.50}',
+          inverseColor: '{red.950}',
+          hoverColor: '{red.100}',
+          activeColor: '{red.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
+      },
+    },
+  },
+});
